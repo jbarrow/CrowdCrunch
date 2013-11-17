6951f0717472a182aa436f9c6b4a62713a477a4f
Joseph Barrow <joseph.d.barrow@gmail.com>
2013-11-16 19:51:34 -0500

    Added basic twilio functions

--- dev/CrowdCrunch/Twilio/texting.py
+++ dev/CrowdCrunch/Twilio/texting.py
@@ -1,7 +1,32 @@
 from twilio.rest import TwilioRestClient
+from twilio import twiml
+
+from string import digits
+from random import choice
+
+#from CrowdCrunch import settings
+#from cruncher.models import *
 
 account = "AC61b168a14cd3c4c2d480006903327506"
 token = "3dc14327f5d7a9e883702bdbbe2648aa"
 client = TwilioRestClient(account, token)
 
-message = client.messages.create(to="5712257068", from_="+17039978527", body="Fuck yeah!")
+def generate_token():
+	return ''.join(choice(digits) for i in xrange(4))
+
+def send_verify_token(number):
+	"""
+	It generates a token, sends it to the number, and returns
+	the token to be stored in the databse
+	"""
+	token = generate_token()
+	client.messages.create(to=number, from_="+17039978527", body="Your token is: " + token)
+	return token
+
+def dont_respond():
+	return str(twiml.Response())
+
+def respond_with_message(message):
+	r = twiml.Response()
+	r.message(message)
+	return str(r)
