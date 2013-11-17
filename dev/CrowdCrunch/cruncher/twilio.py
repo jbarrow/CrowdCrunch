b08677c5619c568f4d1b2a5d7f28c4f24c2ec5c2
Joseph Barrow <joseph.d.barrow@gmail.com>
2013-11-16 21:07:07 -0500

    Finished some twilio stuff

--- dev/CrowdCrunch/cruncher/twilio.py
+++ dev/CrowdCrunch/cruncher/twilio.py
@@ -1,10 +1,18 @@
 from django.views.generic import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 from django.http import HttpResponse
 from texting.texting import *
+from cruncher.models import *
 
 class TwilioView(View):
 	def post(self, request, **kwargs):
-		# pass
-		# send_verify_token("6787630677")
-		return HttpResponse("Hello, world.")
+		user = UserProfile.GetFromPhone(request.POST["From"])
+		if(user == False):
+			return HttpResponse(respond_with_message("You aren't a CrowdCrunch member. Sign up at http://crowdcrunch.com!"))
+		return HttpResponse(dont_respond())
+
+	@method_decorator(csrf_exempt)
+	def dispatch(self, *args, **kwargs):
+		return super(TwilioView, self).dispatch(*args, **kwargs)
